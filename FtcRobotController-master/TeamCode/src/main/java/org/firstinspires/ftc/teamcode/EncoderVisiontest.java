777e3f7c56bc5dce9285e0a376952fb53297a1ff
ShubhamPatilsd <55328098+ShubhamPatilsd@users.noreply.github.com>
2020-11-09 19:12:36 -0800

    test again

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderVisiontest.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderVisiontest.java
@@ -152,17 +152,6 @@ public class EncoderVisiontest extends LinearOpMode {
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-<<<<<<< HEAD
-        
-        //sleep(2000);
-        if(pipeline.position== SkystoneDeterminationPipeline.RingPosition.ONE) {
-            robot.frontright.setPower(1.0);
-        }
-        if(pipeline.position== SkystoneDeterminationPipeline.RingPosition.FOUR) {
-            robot.frontright.setPower(0.0);
-            telemetry.addData("Count of ring bois: ","Sup gamer pog there is four rings yeet");
-            telemetry.update();
-=======
         sleep(2000);
         while(opModeIsActive()) {
             if (pipeline.position == SkystoneDeterminationPipeline.RingPosition.ONE) {
